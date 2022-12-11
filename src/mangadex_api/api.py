12fa3b1cdb91d9b45dd66b0ab3b631a787ba339f
Kevin <LordTaitex@gmail.com>
2022-12-10 20:54:47 -0600

    Added reasonable magna name. pt3

--- src/mangadex_api/api.py
+++ src/mangadex_api/api.py
@@ -1,12 +1,12 @@
-import manga_search_api
+# import manga_search_api
 
-BASE_URL = "https://api.mangadex.org"
-temp_manga_title = "blue lock"   
+# BASE_URL = "https://api.mangadex.org"
+# temp_manga_title = "blue lock"   
 
 
-if __name__ == "__main__":
-    manga_search_obj = manga_search_api.MangaTitle()
-    manga_search_obj.search_manga_title()
+# if __name__ == "__main__":
+#     manga_search_obj = manga_search_api.MangaTitle()
+#     manga_search_obj.search_manga_title()
 
-    # Temporary: 
-    user_input = input("Enter manga names to be stored into database: ")
+#     # Temporary: 
+#     user_input = input("Enter manga names to be stored into database: ")
